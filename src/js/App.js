48833abe301cfaa04cc3c3c873922d850e70da32
khaled <elgendy1204@gmail.com>
2017-09-16 22:40:21 +0200

    starting navbar code

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import Login from './containers/Login';
+import Navbar from './components/Navbar';
 import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <Navbar />
 
         <Link to='/Login'>
           To Login
---
+++ src/js/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+
+    componentDidMount() {
+        $(".button-collapse").sideNav();
+    }
+
+    render() {
+        return (
+          <nav>
+            <div className="nav-wrapper">
+              <a href="#!" className="brand-logo">Logo</a>
+              <a href="#" data-activates="mobile-demo" className="button-collapse">
+                <i className="material-icons">menu</i>
+              </a>
+              <ul className="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+              </ul>
+              <ul className="side-nav" id="mobile-demo">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+              </ul>
+            </div>
+          </nav>
+        );
+    }
+
+}
+
+export default Navbar;
