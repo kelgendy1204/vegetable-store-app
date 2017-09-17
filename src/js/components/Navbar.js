7763512a9685b37ecffb3704e9a571d44c9140ee
Khaled Saad <elgendy1204@gmail.com>
2017-09-17 12:06:43 +0200

    fix navbar fonts

--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -3,17 +3,20 @@ import React, { Component } from 'react';
 class Navbar extends Component {
 
     componentDidMount() {
-        $(".button-collapse").sideNav();
+        $(".button-collapse").sideNav(
+        {
+            menuWidth: 300,
+            closeOnClick: true,
+            edge: 'right',
+        });
     }
 
     render() {
         return (
-          <nav>
+          <nav className="fixed">
             <div className="nav-wrapper">
-              <a href="#!" className="brand-logo">Logo</a>
-              <a href="#" data-activates="mobile-demo" className="button-collapse">
-                <i className="material-icons">menu</i>
-              </a>
+              <a href="#!" className="brand-logo left">Logo</a>
+              <a href="#" data-activates="mobile-demo" className="button-collapse right"><i className="material-icons">menu</i></a>
               <ul className="right hide-on-med-and-down">
                 <li><a href="sass.html">Sass</a></li>
                 <li><a href="badges.html">Components</a></li>
