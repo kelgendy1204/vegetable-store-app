f280da979121b6c9d8d2f2139ef30e567bec2ae7
mado721 <mado.mazika2day@gmail.com>
2017-09-25 15:51:35 +0200

    add prodact page

--- src/js/components/ItemDetail.js
+++ src/js/components/ItemDetail.js
@@ -10,7 +10,45 @@ class ItemDetail extends Component {
     render() {
         return (
             <div className="item-detail">
-                ItemDetail
+                <div className="items-Product">
+                    <div className="item ">
+                        <div className="content1">
+                            <p>تفاح امريكي </p>
+                            <p className="sc">مستورد وذو جودة عالية</p>
+                        </div>
+                        <div className="a5tyar">
+                            <div className="right-data">
+                                <div className="up-data">
+                                    <div className="content">
+                                        <p><span className="font">%</span>10</p>
+                                    </div>
+                                </div>
+                                <div className="down-data">
+                                    <img src="assets/images/tmatm.jpg" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="content2">
+                            <div className="one">
+                                <p className="fir">35,5 جنية</p>
+                                <p className="sc">40,90 جنية</p>
+                            </div>
+                            <div className="two">
+                                <img src="assets/images/12.svg" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="button">
+                    <div className="content">
+                        <img src="assets/images/1.svg" />
+                        <p>اضف الي السلة</p>
+                    </div>
+                    <div className="hart">
+                        <img src="assets/images/11.svg" />
+                    </div>
+                </div>
+
             </div>
         );
     }
