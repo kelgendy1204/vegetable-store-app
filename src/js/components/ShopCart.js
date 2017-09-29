71ce95079c943e106a298d1a4358aab02e9fbb84
mado721 <mado.mazika2day@gmail.com>
2017-09-29 16:10:32 +0200

    add cart page html&scss

--- src/js/components/ShopCart.js
+++ src/js/components/ShopCart.js
@@ -11,7 +11,108 @@ class ShopCart extends Component {
 
         return (
             <div className='shop-cart'>
+                <div className="items-cart">
+                    <div className="item-cart">
+                        <div className="right-data-cart"><img src="./assets/images/khyar.jpg" /></div>
+                        <div className="center-data-cart">
+                            <div className="top-data">
+                                <div className="center-data">
+                                    <p className=""> كيلو خيار بلدي </p>
+                                    <p className="two"> كيلو خيار بلدي </p>
+                                </div>
+                            </div>
+                            <div className="bottom-data">
+                                <div className="content">
+                                    <div className="border1"><p>+</p></div>
+                                    <div className="border2"><p>1</p></div>
+                                    <div className="border1"><p>-</p></div>
 
+                                </div>
+                            </div>
+                        </div>
+                        <div className="left-data-cart">
+                            <div className="content-data-left"><div className="content"><img src="./assets/images/1.svg" /></div></div>
+                            <p>اجمالي الصنف</p>
+                            <p className="one">105,99</p>
+                            <p className="two">جنية</p>
+                        </div>
+                    </div>
+                    <div className="item-cart">
+                        <div className="right-data-cart"><img src="./assets/images/khyar.jpg" /></div>
+                        <div className="center-data-cart">
+                            <div className="top-data">
+                                <div className="center-data">
+                                    <p className=""> كيلو خيار بلدي </p>
+                                    <p className="two"> كيلو خيار بلدي </p>
+                                </div>
+                            </div>
+                            <div className="bottom-data">
+                                <div className="content">
+                                    <div className="border1"><p>+</p></div>
+                                    <div className="border2"><p>1</p></div>
+                                    <div className="border1"><p>-</p></div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="left-data-cart">
+                            <div className="content-data-left"><div className="content"><img src="./assets/images/1.svg" /></div></div>
+                            <p>اجمالي الصنف</p>
+                            <p className="one">105,99</p>
+                            <p className="two">جنية</p>
+                        </div>
+                    </div>
+                    <div className="item-cart">
+                        <div className="right-data-cart"><img src="./assets/images/khyar.jpg" /></div>
+                        <div className="center-data-cart">
+                            <div className="top-data">
+                                <div className="center-data">
+                                    <p className=""> كيلو خيار بلدي </p>
+                                    <p className="two"> كيلو خيار بلدي </p>
+                                </div>
+                            </div>
+                            <div className="bottom-data">
+                                <div className="content">
+                                    <div className="border1"><p>+</p></div>
+                                    <div className="border2"><p>1</p></div>
+                                    <div className="border1"><p>-</p></div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="left-data-cart">
+                            <div className="content-data-left"><div className="content"><img src="./assets/images/1.svg" /></div></div>
+                            <p>اجمالي الصنف</p>
+                            <p className="one">105,99</p>
+                            <p className="two">جنية</p>
+                        </div>
+                    </div>
+                    <div className="item-cart">
+                        <div className="right-data-cart"><img src="./assets/images/khyar.jpg" /></div>
+                        <div className="center-data-cart">
+                            <div className="top-data">
+                                <div className="center-data">
+                                    <p className=""> كيلو خيار بلدي </p>
+                                    <p className="two"> كيلو خيار بلدي </p>
+                                </div>
+                            </div>
+                            <div className="bottom-data">
+                                <div className="content">
+                                    <div className="border1"><p>+</p></div>
+                                    <div className="border2"><p>1</p></div>
+                                    <div className="border1"><p>-</p></div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="left-data-cart">
+                            <div className="content-data-left"><div className="content"><img src="./assets/images/1.svg" /></div></div>
+                            <p>اجمالي الصنف</p>
+                            <p className="one">105,99</p>
+                            <p className="two">جنية</p>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         );
