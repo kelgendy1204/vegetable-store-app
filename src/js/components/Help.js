436abac60dd08a37aa67aeb3a5aeffea763695f2
khaled <elgendy1204@gmail.com>
2017-10-14 12:04:40 +0200

    finish help page

--- src/js/components/Help.js
+++ src/js/components/Help.js
@@ -6,10 +6,43 @@ class Help extends Component {
         super(props);
     }
 
+    componentDidMount(){
+        $(document).ready(function(){
+            $('.collapsible').collapsible();
+        });
+    }
+
+    componentWillUnmount(){
+        $('.collapsible').collapsible('destroy');
+    }
+
     render() {
         return (
-            <div className="help-page">
-                help
+            <div className="help-page container">
+                <div className="row">
+                    <div className="col s12">
+                        <ul className="collapsible" data-collapsible="accordion">
+                           <li>
+                             <div className="collapsible-header">
+                                 First
+                             </div>
+                             <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                           </li>
+                           <li>
+                             <div className="collapsible-header">
+                                 Second
+                             </div>
+                             <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                           </li>
+                           <li>
+                             <div className="collapsible-header">
+                                 Third
+                             </div>
+                             <div className="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                           </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         );
     }
